fbcb760b1f950fec04e70226f4fe1b0ec4d4918f
Nikita Skhabuitskyi <nikitaskh211@gmail.com>
2025-02-05 01:09:28 +0200

    Learned about variables.

---
+++ Lesson1_Variables/main.rs
@@ -0,0 +1,6 @@
+// main entry point
+fn main() {
+    // just a difference between 'print' and 'println'
+    print!("Message without newline.\n");
+    println!("Message with newline.");
+}
---
+++ Lesson1_Variables/problem1.rs
@@ -0,0 +1,22 @@
+/* -- original = unsolved
+// Fix the error below with least amount of modification to the code
+fn main() {
+    let x: i32; // Uninitialized but used, ERROR !
+    let y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
+*/
+
+// Fix the error below with least amount of modification to the code
+fn main() {
+    let x: i32 = 5; // Uninitialized but used, ERROR !
+    let _y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
+
+// conclusion 1 = variables must be initialized to be used
+// conclusion 2 = variables that are uninitialized and unused must have an underscore
